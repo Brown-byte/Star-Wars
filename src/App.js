a67e0cad50c223dcecd585daac595a90d1aa8c93
Brown-byte <ellatsalibrown15@gmail.com>
2023-04-19 12:35:20 +0000

    commited

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FilmList from './Components/FilmList.js';
+
+function App() {
+  return (
+    <div className="App">
+     <FilmList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FilmList.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
+const FilmList = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            const response = await axios.get(`https://swapi.dev/api/films`);
+            setData(response.data.results);
+            setError(null);
+          } catch (err) {
+            setError(err.message);
+            setData(null);
+          } finally {
+            setLoading(false);
+          }
+        };
+        getData();
+    }, []);
+      
+    console.log(data)
+    const reduceLength = (text, maxLength) => {
+        if (text.length > maxLength) {
+            return text.slice(0, maxLength).trim() + '...';
+        } else {
+            return text
+        }
+    }
+ 
+  return (
+    <div>
+        {data && data.map(function(movies){
+            return (
+                <div  key = {movies.id} className="FilmList">
+                    <div>
+                      
+                      <h3 className='title'>{movies.title}</h3>
+                      <p className='when'>{new Date(movies.release_date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</p>
+                      <p className='opening-crawl'>{reduceLength(movies.opening_crawl, 260)}</p>
+                      <hr/>
+                      <p className='info'>More info</p>
+                    
+                    </div>
+                    
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default FilmList;
