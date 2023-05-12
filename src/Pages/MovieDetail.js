6251f0a0513277e9d31f017173d22852f851334f
Brown-byte <ellatsalibrown15@gmail.com>
2023-05-12 19:27:18 +0000

    Update

--- src/Pages/MovieDetail.js
+++ src/Pages/MovieDetail.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const MovieDetail = () => {
-  return (
-    <div>MovieDetail</div>
-  )
-}
+  const [allCharacters, setAllCharacters]  = useState([]);
+  const navigate = useNavigate();
+  const { state } = useLocation();
 
-export default MovieDetail
+  useEffect(() => { 
+    const characterPromises = state[0].characters.map((url) => axios.get(url));
+    Promise.all(characterPromises)
+    .then((responses) => {
+      const characters = responses.map((response) => response.data.name);
+      setAllCharacters(characters);
+      console.log(characters);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, [state]);
+
+  return (
+    <div style={{ backgroundColor: "gray" }}>
+      {state.map((movie) => (
+      <div key={movie.episode_id}>
+        <p onClick={() => navigate ("/")}>Back to List</p>
+        <h1 style={{ color: "#fff" }}>{movie.title}</h1>
+        <p>Director: {movie.director}</p>
+        <p>Producer: {movie.producer}</p>
+        
+        <div>
+        <h4>Description</h4>
+        <p>{movie.opening_crawl}</p>
+        </div>
+
+        <div>
+          {allCharacters.map((name) =>{
+            <li>{name}</li>
+          })}
+        </div>
+      
+      </div>
+      ))}
+       
+    </div>
+  );
+};
+
+export default MovieDetail;
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
 
 ]);
 
-const root= ReactDOM.createRoot(document.getElementById);
+const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
